5d2525d0608c82403b93ef1e6f2401374f20e97a
Matthew King <automatthew@gmail.com>
2014-10-16 12:45:44 -0500

    comments for Script

--- lib/coin-op/bit/script.rb
+++ lib/coin-op/bit/script.rb
@@ -1,11 +1,32 @@
 
 module CoinOp::Bit
 
+  # A wrapper class to make it easier to read and write Bitcoin scripts.
+  # Provides a sane #to_json method.
   class Script
     include CoinOp::Encodings
 
+    # Accessor for the "native" ::Bitcoin::Script script instance
     attr_reader :native
 
+    # Takes either a String or a Hash as its argument.
+    #
+    # A String argument will be parsed as a human readable script.
+    #
+    # A Hash argument specifies a script using one of several possible
+    # keys:
+    #
+    # * :string
+    # * :blob
+    # * :hex
+    # * :address
+    # * :public_key
+    # * :public_keys, :needed
+    # * :signatures
+    #
+    # The name of the crypto-currency network may also be specified.  It
+    # defaults to :testnet3.  Names supplied in this manner must correspond
+    # to the names in the ::Bitcoin::NETWORKS Hash.
     def initialize(options)
       # Doing the rescue in case the input argument is a String.
       network_name = (options[:network] || :testnet3) rescue :testnet3
@@ -61,8 +82,11 @@ module CoinOp::Bit
     def type
       case self.native.type
       when :hash160
+        # Pay to address, because an "address" is really just the hash
+        # of a public key.
         :pubkey_hash
       when :p2sh
+        # Pay to Script Hash
         :script_hash
       else
         self.native.type
@@ -84,14 +108,20 @@ module CoinOp::Bit
       Bitcoin.hash160(@hex)
     end
 
+    # Generate the script that uses a P2SH address.
+    # Used for an Output's scriptPubKey value.
     def p2sh_script
       self.class.new Bitcoin::Script.to_p2sh_script(self.hash160)
     end
 
+    # Generate the P2SH address for this script.
     def p2sh_address
       Bitcoin.hash160_to_p2sh_address(self.hash160)
     end
 
+    # Generate a P2SH script_sig for the current script, using the
+    # supplied options, which will, in the case of a multisig input,
+    # be {:signatures => array_of_signatures}.
     def p2sh_sig(options)
       string = Script.new(options).to_s
       Bitcoin::Script.binary_from_string("#{string} #{self.to_hex}")
